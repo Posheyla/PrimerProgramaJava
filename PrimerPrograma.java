4674d91c558bafce643c56739e744d8b290e85d1
Posheyla <luisa.virgo.98@hotmail.com>
2022-02-19 16:29:08 -0500

    Primer asignación Java

---
+++ PrimerPrograma.java
@@ -0,0 +1,7 @@
+public class PrimerPrograma{
+    public static void main(String args[]){
+        System.out.println("Mi nombre es Luisa Posheyla");
+        System.out.println("Tengo 23 años de edad");
+        System.out.println("Mi ciudad natal es Cajamarca, PE");
+    }
+}
